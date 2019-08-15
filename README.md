de6baa2c39f183db97db98f876c9390b97f74967
hew <mathujones@gmail.com>
2019-08-15 15:28:02 -0700

    feat: updates

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # xstate-react-global-context-example
 
 Shows how to strictly use xstate to handle your app's global state. 
+
+## Getting started
+
+`yarn`
+`yarn run start`
--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -31496,7 +31496,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56266" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61203" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
